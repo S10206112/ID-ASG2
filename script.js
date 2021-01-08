153aedc5af4cafd9f6f8748dea2525ba2ba414c8
S10206112 <73154832+S10206112@users.noreply.github.com>
2021-01-08 21:12:23 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ $(document).ready(function () {
     fetch('http://newsapi.org/v2/top-headlines?country=sg&apiKey=2358a271cd264c639fc86f2c6967aaf5')
   .then(response => response.json())
   .then(data => console.log(data));
+function myFunction(){
+  fetch(url)
+    .then(response => response.json()) 
+    .then(function(data){
+      console.log(data)
+      console.log(articles["name"]);
+        $("#fullname").html(`${articles.author} ${articles.title}`);
+    });
